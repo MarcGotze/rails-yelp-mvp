43ceeee317d56c7c82b487cba83f71a6761d27ea
Marc Gotze marc.gotze@gmail.com <marc.gotze@gmail.com>
2023-08-16 17:04:10 +0200

    Yelp mvp WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+puts "Creating restaurants..."
+puts "==========================================="
+10.times do
+  new_restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+  new_restaurant.save!
+  5.times do
+    Review.create!(
+      rating: Faker::Number.between(from: 0, to: 5),
+      content: Faker::Restaurant.review,
+      restaurant: new_restaurant
+    )
+  end
+  puts new_restaurant.name
+end
+puts "==========================================="
+puts "Completed !"
